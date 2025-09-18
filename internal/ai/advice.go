24b0ae40cf670f69d3a6117137abd8268a2886c9
ashvin <142579833+ASH-WIN-10@users.noreply.github.com>
2025-09-18 23:13:30 +0530

    add configuration for gemini

--- internal/ai/advice.go
+++ internal/ai/advice.go
@@ -2,21 +2,42 @@ package ai
 
 import (
 	"context"
+	"time"
 
 	"google.golang.org/genai"
 )
 
 func (c *AIClient) GenerateLegalAdvice(prompt string) (string, error) {
+	systemInstruction := `
+        You are a helpful legal assistant.
+        Provide clear and concise legal advice based on the user's prompt.
+        Ensure that your advice is accurate and easy to understand.
+        Don't provide any disclaimers or additional information beyond the legal advice requested.
+        Dont't use technical terms or jargon that a layperson might not understand.
+        Keep your responses brief and to the point.
+    `
+	temperature := float32(0.3)
+	prompt = prompt + "\n\nDo not mention their region or country name unless explicitly asked like (In India, In USA etc.). "
+
+	config := &genai.GenerateContentConfig{
+		MaxOutputTokens:   500,
+		Temperature:       &temperature,
+		SystemInstruction: genai.NewContentFromText(systemInstruction, ""),
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
+	defer cancel()
+
 	result, err := c.client.Models.GenerateContent(
-		context.Background(),
+		ctx,
 		"gemini-2.5-flash",
 		genai.Text(prompt),
-		nil,
+		config,
 	)
 
 	if err != nil {
 		return "", err
 	}
 
-	return result.Candidates[0].Content.Parts[0].Text, nil
+	return result.Text(), nil
 }
