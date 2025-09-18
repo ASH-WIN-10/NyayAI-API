29395c99d367d1aac3ebdb6be8a2adc04ce441e9
ashvin <142579833+ASH-WIN-10@users.noreply.github.com>
2025-09-18 21:48:35 +0530

    add more error responses

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -28,4 +29,18 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 
 	message := "the server encounterd a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,6 +8,10 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	return router
 }
