a4f7caae7ab3d4e80fc9ab7b454740df392536bb
ashvin <142579833+ASH-WIN-10@users.noreply.github.com>
2025-09-19 22:09:48 +0530

    add request logger middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,8 +13,7 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-
 	router.HandlerFunc(http.MethodPost, "/v1/advice", app.getLegalAdvice)
 
-	return router
+	return app.logRequest(router)
 }
